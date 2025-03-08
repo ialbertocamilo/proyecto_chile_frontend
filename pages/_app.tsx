50426cad2e694f8cc9dbe678232ffb1c5058443d
Alberto Camilo Rodriguez Vizcarra <svg.z32@gmail.com>
2025-03-08 16:28:19 -0500

    fix: changing style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "../public/assets/css/globals.css"; // Asegúrate de que existe este archivo
+import '../public/assets/css/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

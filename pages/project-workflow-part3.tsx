fc9d229d1cd59af268a83ea3d721d75ad48bc185
Alberto Camilo Rodriguez Vizcarra <svg.z32@gmail.com>
2025-03-12 15:11:50 -0500

    fix: table striped

--- pages/project-workflow-part3.tsx
+++ pages/project-workflow-part3.tsx
@@ -913,7 +913,7 @@ const ProjectWorkflowPart3: React.FC = () => {
           {tabStep4 === "muros" && (
             <div style={{ overflowX: "auto" }}>
               <table
-                className="table table-bordered table-striped"
+                className="table table-bordered "
                 style={{ width: "100%", minWidth: "600px" }}
               >
                 <thead>
@@ -1071,7 +1071,7 @@ const ProjectWorkflowPart3: React.FC = () => {
           {tabStep4 === "techumbre" && (
             <div style={{ overflowX: "auto" }}>
               <table
-                className="table table-bordered table-striped"
+                className="table table-bordered "
                 style={{ width: "100%", minWidth: "600px" }}
               >
                 <thead>
@@ -1230,7 +1230,7 @@ const ProjectWorkflowPart3: React.FC = () => {
           )}
           ;
           {tabStep4 === "pisos" && (
-            <table className="table table-bordered table-striped">
+            <table className="table table-bordered">
               <thead>
                 <tr>
                   <th
@@ -1411,7 +1411,7 @@ const ProjectWorkflowPart3: React.FC = () => {
             </table>
           )}
           {tabStep4 === "ventanas" && (
-            <table className="table table-bordered table-striped">
+            <table className="table table-bordered ">
               <thead>
                 <tr>
                   <th
@@ -1515,7 +1515,7 @@ const ProjectWorkflowPart3: React.FC = () => {
             </table>
           )}
           {tabStep4 === "puertas" && (
-            <table className="table table-bordered table-striped">
+            <table className="table table-bordered">
               <thead>
                 <tr>
                   <th
@@ -1687,7 +1687,7 @@ const ProjectWorkflowPart3: React.FC = () => {
         <div className="mb-3">
           <div style={{ height: "400px", overflowY: "scroll" }}>
             <table
-              className="table table-bordered table-striped"
+              className="table table-bordered "
               style={{ textAlign: "center" }}
             >
               <thead>
@@ -2077,7 +2077,7 @@ const ProjectWorkflowPart3: React.FC = () => {
           <div></div>
         </div>
         <div style={{ height: "390px", overflowY: "scroll" }}>
-          <table className="table table-bordered table-striped">
+          <table className="table table-bordered ">
             <thead>
               <tr>
                 <th style={{ ...stickyHeaderStyle1 }}>ID</th>

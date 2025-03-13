ad63bb8cac46edaad3ac80511c6cc50913104731
Luis Alatta <newluisalattago@gmail.com>
2025-03-12 22:11:51 -0500

    color letra migajas

--- src/components/common/Breadcrumb.tsx
+++ src/components/common/Breadcrumb.tsx
@@ -14,23 +14,31 @@ interface BreadcrumbProps {
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ items }) => {
   return (
     <div className="container-fluid row col-sm-6 p-0 page-title">
-      <ol className="breadcrumb" style={{ textDecoration: 'none' }}>
+      <ol
+        className="breadcrumb"
+        style={{ textDecoration: "none", color: "#2ab0c5" }}
+      >
         <li className="breadcrumb-item">
-          <Link href="/" style={{ textDecoration: 'none' }}>
+          <Link href="/" style={{ textDecoration: "none", color: "#2ab0c5" }}>
             <svg className="stroke-icon">
               <use href="../assets/svg/icon-sprite.svg#stroke-home"></use>
             </svg>
           </Link>
         </li>
         {items.map((item, index) => (
-          <li 
-            key={index} 
-            className={`breadcrumb-item ${item.active ? 'active' : ''}`}
+          <li
+            key={index}
+            className={`breadcrumb-item ${item.active ? "active" : ""}`}
           >
             {item.href ? (
-              <Link href={item.href} style={{ textDecoration: 'none' }}>{item.title}</Link>
+              <Link
+                href={item.href}
+                style={{ textDecoration: "none", color: "#2ab0c5" }}
+              >
+                {item.title}
+              </Link>
             ) : (
-              item.title
+              <span style={{ color: "#2ab0c5" }}>{item.title}</span>
             )}
           </li>
         ))}

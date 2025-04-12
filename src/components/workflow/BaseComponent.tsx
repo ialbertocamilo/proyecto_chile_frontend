48ccadf13c1ce2f64578173cf27c7a4ff031e77c
garosoro <gabrielr.sotorojas@gmail.com>
2025-04-12 14:48:23 -0500

    add baseline for refactoring

---
+++ src/components/workflow/BaseComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface YourComponentNameProps {}
+
+const YourComponentName: React.FC<YourComponentNameProps> = ({}) => {
+  return (
+    <div>
+      <h2>dsa</h2>
+    </div>
+  );
+};
+
+export default YourComponentName;

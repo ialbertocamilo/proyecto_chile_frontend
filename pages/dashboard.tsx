a0f9e24fb7193ece24635e4199fc5749ed747490
Alberto Camilo Rodriguez Vizcarra <svg.z32@gmail.com>
2025-08-21 12:53:08 -0500

    fix: countries

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -121,7 +121,7 @@ const DashboardPage: React.FC = () => {
 
     // Datos dummy para selects
     const years = ['','2022', '2023', '2024', '2025'];
-    const countries = ['','Chile', 'Colombia', 'Ecuador', 'México', 'Perú'];
+    const countries = ['','Perú', 'Ecuador', 'Chile', 'Colombia'];
     const zones = ['','A', 'B', 'C', 'D','E','F'];
     const typologies = ['','Residencial en altura', 'Residencial en extensión', 'Educación', 'Salud', 'Comercio', 'Servicios (oficinas)'];
 
